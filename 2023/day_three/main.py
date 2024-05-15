23a5d1f07de21a450e9b6dddd9db9a79efaa3b76
d00ksky <debski.jakub@gmail.com>
2024-05-15 21:08:20 +0200

    trying

--- 2023/day_three/main.py
+++ 2023/day_three/main.py
@@ -1,24 +1,24 @@
 input_file = open("test", "r")
 lines = input_file.readlines()
 
-lol = []
+grid = [list(line.strip()) for line in lines]
 
+print(grid[1][1])
 
-# Create grid representation of given data
-# searching for each cell if it is digit and if it's a part of bigger number
-# searching if that number has adjacent cells with symbols
-# marking visited cells and keeping track of them
-#
-# Building Numbers: When a digit is found, check its adjacent cells to see if they also contain digits. If they do, continue this process recursively, building the number as you go along until no adjacent digits are left. This would help in identifying complete numbers like "467", "35", etc.
-# 
+def is_adjacent_to_a_symbol(grid, x, y):
+    directions = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+            if grid[nx][ny] in '*#+$':
+                return True
+    return False
 
+total_sum = 0
 
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        if grid[i][j].isdigit() and is_adjacent_to_a_symbol(grid, i, j):
+            total_sum += int(grid[i][j])
 
-# building grid as list of lists:
-
-for line in lines:
-    lol.append(line)
-print(lol)
-
-
-for 
+print(total_sum)
